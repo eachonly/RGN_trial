fd81c2a57297263222390840032eeba7093e09cc
Youwei Qin <c3149434@uon.edu.au>
2018-10-21 01:20:02 +0800

    add batch file for ifort
    add batch file for ifort

---
+++ RGN/PROJ/rosenbrock/rosenbrock_ifort.bat
@@ -0,0 +1,18 @@
+REM compile and run RGN on rosenbrock
+:: Instructons: 1) add PATH for ifort and run ifortvars.bat
+::				2) compile: ifort -c ..\..\SRC_RGN\constantsMod.f90 	
+::				   			ifort -c ..\..\SRC_RGN\rgn.f90
+::							ifort -c ..\..\SRC_DEMO\rosenbrock\rgnMain_Rosenbrock.f90
+::              3) link: ifort -o rosenbrock_demo.exe *.o
+::              4) run: rosenbrock_demo.exe
+
+echo compile rosenbrock demo with gfortran
+Set "ifortvars_path=C:\Program Files (x86)\Intel\Composer XE 2011 SP1\bin"
+CALL "%ifortvars_path%\ifortvars.bat" ia32
+ifort -c ..\..\SRC_RGN\constantsMod.f90
+ifort -c ..\..\SRC_RGN\rgn.f90
+ifort -c ..\..\SRC_DEMO\rosenbrock\rgnMain_Rosenbrock.f90
+ifort -o rosenbrock_demo.exe *.obj
+echo run rosenbrock demo
+rosenbrock_demo.exe
+PAUSE
