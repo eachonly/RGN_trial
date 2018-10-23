a3e8a64f3c23e69106ec60e50a6f72cbdc85fa2b
Youwei Qin <c3149434@uon.edu.au>
2018-10-23 22:01:10 +0800

    Revert "add option for 32/64 bit complier"
    This reverts commit 7e0fa51e78c3ffb42ecaf9a56b73176f5c763d3e.

--- RGN/PROJ/hymod/hymod_gfortran.bat
+++ RGN/PROJ/hymod/hymod_gfortran.bat
@@ -3,7 +3,6 @@ REM compile and run RGN on hymod
 ::				2) compile: gfortran -c -ffree-line-length-none ..\..\SRC_RGN\constantsMod.f90 ..\..\SRC_RGN\rgn.f90 ..\..\SRC_DEMO\hymod\hydroDataMod.f90 ..\..\SRC_DEMO\hymod\hymod.f90 ..\..\SRC_DEMO\hymod\rgnMain_Hymod.f90
 ::              3) link: gfortran  -o hymod_demo.exe *.o
 ::              4) run: hymod_demo.exe
-:: The verion of executable file will depends on the gfortran version (32 bit or 64 bit) installed
 
 echo compile rosenbrock demo with gfortran
 ::add gfortran path to environmental variable, set "PATH=%PATH%" 
