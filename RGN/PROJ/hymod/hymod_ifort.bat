c38cd454c9cef4ac1b763106b9db1a98c624f179
Youwei Qin <c3149434@uon.edu.au>
2018-10-23 22:07:38 +0800

    revert the changes, and then redo retest x64
    revert the changes, and then redo retest x64

--- RGN/PROJ/hymod/hymod_ifort.bat
+++ RGN/PROJ/hymod/hymod_ifort.bat
@@ -7,10 +7,11 @@ REM compile and run RGN on hymod
 ::							ifort -c ..\..\SRC_DEMO\hymod\rgnMain_Hymod.f90
 ::              3) link: ifort -o hymod_demo.exe *.o
 ::              4) run: hymod_demo.exe
+:: To compile 32 bit executable file, please replace "intel64" by "ia32"
 
 echo compile hymod demo with gfortran
 Set "ifortvars_path=C:\Program Files (x86)\Intel\Composer XE 2011 SP1\bin"
-CALL "%ifortvars_path%\ifortvars.bat" ia32
+CALL "%ifortvars_path%\ifortvars.bat" intel64
 ifort -c ..\..\SRC_RGN\constantsMod.f90
 ifort -c ..\..\SRC_RGN\rgn.f90
 ifort -c ..\..\SRC_DEMO\hymod\hydroDataMod.f90
--- RGN/PROJ/rosenbrock/rosenbrock_ifort.bat
+++ RGN/PROJ/rosenbrock/rosenbrock_ifort.bat
@@ -5,10 +5,11 @@ REM compile and run RGN on rosenbrock
 ::							ifort -c ..\..\SRC_DEMO\rosenbrock\rgnMain_Rosenbrock.f90
 ::              3) link: ifort -o rosenbrock_demo.exe *.o
 ::              4) run: rosenbrock_demo.exe
+:: To compile 32 bit executable file, please replace "intel64" by "ia32"
 
 echo compile rosenbrock demo with gfortran
 Set "ifortvars_path=C:\Program Files (x86)\Intel\Composer XE 2011 SP1\bin"
-CALL "%ifortvars_path%\ifortvars.bat" ia32
+CALL "%ifortvars_path%\ifortvars.bat" intel64
 ifort -c ..\..\SRC_RGN\constantsMod.f90
 ifort -c ..\..\SRC_RGN\rgn.f90
 ifort -c ..\..\SRC_DEMO\rosenbrock\rgnMain_Rosenbrock.f90
