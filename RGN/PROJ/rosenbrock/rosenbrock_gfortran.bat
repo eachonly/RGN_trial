7e0fa51e78c3ffb42ecaf9a56b73176f5c763d3e
Youwei Qin <c3149434@uon.edu.au>
2018-10-23 20:57:49 +0800

    add option for 32/64 bit complier
    Add the option for compile 32 and 64 bit version exectuable file

--- RGN/PROJ/rosenbrock/rosenbrock_gfortran.bat
+++ RGN/PROJ/rosenbrock/rosenbrock_gfortran.bat
@@ -3,6 +3,7 @@ REM compile and run RGN on rosenbrock
 ::				2) compile: gfortran -c -ffree-line-length-none ..\..\SRC_RGN\constantsMod.f90 ..\..\SRC_RGN\rgn.f90 ..\..\SRC_DEMO\rosenbrock\rgnMain_Rosenbrock.f90
 ::              3) link: gfortran  -o rosenbrock_demo.exe *.o
 ::              4) run: rosenbrock_demo.exe
+:: The verion of executable file will depends on the gfortran version (32 bit or 64 bit) installed
 
 echo compile rosenbrock demo with gfortran
 ::add gfortran path to environmental variable, set "PATH=%PATH%" 
